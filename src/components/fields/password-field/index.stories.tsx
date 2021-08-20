764b22ae7e16261e2d14c29a68348f976bd262f9
Quincy Kayle <kayleqb@gmail.com>
2021-08-20 13:06:04 -0500

    chore: maybe fix

---
+++ src/components/fields/password-field/index.stories.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { PasswordField } from './index';
+import { FieldProps } from '../utils';
+
+export default {
+    title: 'Fields/Password Field',
+    component: PasswordField,
+    description: `A field for a password`,
+} as Meta;
+
+//👇 We create a “template” of how args map to rendering
+const Template: Story<FieldProps> = (props) => <PasswordField {...props} />;
+
+//👇 Each story then reuses that template
+export const Default = Template.bind({});
+Default.args = {};
