238121372e0c36ba11637a56fd956f8acf7740ea
Quincy Kayle <kayleqb@gmail.com>
2021-08-20 11:57:26 -0500

    chore: maybe fix

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-// jest.config.ts
-import type { Config } from '@jest/types';
-
-// Sync object
-const config: Config.InitialOptions = {
-    verbose: true,
-    setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
-};
-export default config;
